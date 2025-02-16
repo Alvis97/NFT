45232ebfb43990bedac77374669ea071e27e72bd
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2025-02-17 00:11:26 +0100

    installations

--- nft-mint.ts
+++ nft-mint.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
-import react from "react";
 import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { keypairIdentity } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { getKeypairFromEnvironment, getKeypairFromFile } from "@solana-developers/helpers";
 import { promises as fs } from "fs";
 import { clusterApiUrl } from "@solana/web3.js";
+import { createGenericFile } from "@metaplex-foundation/umi";
+
  
 const umi = createUmi(clusterApiUrl("devnet"));
  
@@ -20,11 +21,22 @@ const umiKeypair = umi.eddsa.createKeypairFromSecretKey(localKeypair.secretKey);
 umi.use(keypairIdentity(umiKeypair)).use(mplTokenMetadata());
 
 //Upload random image
-const imagePath = "Random-image.png";
+const imagePath = "nft-mint.png";
 const buffer = await fs.readFile(imagePath);
 let file = createGenericFile(buffer, imagePath, {
     contentType: "image/jpeg",
 });
 const [image] = await umi.uploader.upload([file]); 
+
+//Create uri
+const uri = await umi.uploader.uploadJson({
+    name: "NFT Mint",
+    description: "This is my nft mint description",
+    image,
+  });
+
+console.log("Uri: ", uri);
+
+
 
 
